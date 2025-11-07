821d130c8cae6256ba688a327052158db7714ed8
kalvintn <63025530+kalvintn@users.noreply.github.com>
2025-11-06 21:10:18 -0500

    Implement playRound for main game logic. Add helper fn printScore.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Note: In JavaScript, function declarations (including the body) are hoisted
 
-
+// Control Flow
+// playGame() loops through multiple rounds
+// game variables defined inside playGame()
 
 
 
@@ -32,5 +34,48 @@ function getHumanChoice(){
             humanChoice !== 'scissors')
         { humanChoice = prompt("Please try again. Enter 'rock', 'paper', or 'scissors'."); }
 
+    // standardize output
+    humanChoice = humanChoice.toLowerCase();
     return humanChoice;
+}
+
+
+// Game Variables
+let humanScore = 0;
+let computerScore = 0;
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection);
+/* Play Round : Compare human & computer results, dole points. */
+function playRound(humanChoice, computerChoice){
+    // Case such that a tie occurs
+    if(humanChoice === computerChoice){
+        console.log(`tie! ${humanChoice} meets ${computerChoice}.`);
+    }
+    // case such that player wins
+    else if(humanChoice == "rock" && computerChoice == "scissors" ||
+            humanChoice == "scissors" && computerChoice == "paper" ||
+            humanChoice == "paper" && computerChoice == "rock")
+    {   console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+        humanScore++;
+    }
+    // case such that computer wins
+     else if(computerChoice == "rock" && humanChoice == "scissors" ||
+            computerChoice == "scissors" && humanChoice == "paper" ||
+            computerChoice == "paper" && humanChoice == "rock")
+    {   console.log(`Computer wins! ${computerChoice} beats ${humanChoice}.`);
+        computerScore++;
+    }
+
+    // call to helper
+    printScore(humanScore, computerScore);
+}
+
+
+
+
+
+/* Helper function : print score */
+function printScore(humanScore, computerScore){
+    console.log(`Score: Human ${humanScore} / Computer ${computerScore}`);
 }
